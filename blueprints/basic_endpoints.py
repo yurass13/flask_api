eecd63fc00436c6d3c201e9ddfdd801c08f789cf
Yury Stolpov <yurass130@gmail.com>
2023-10-21 18:45:42 +0300

    Feat: REST API for variables[GET, POST, PUT]

--- blueprints/basic_endpoints.py
+++ blueprints/basic_endpoints.py
@@ -2,51 +2,51 @@ import os
 
 from flask import abort, Blueprint, request
 from redis import Redis
-
+from flask import jsonify
 
 blueprint = Blueprint('api', __name__, url_prefix='/v1')
 redis = Redis(host=os.environ.get("REDIS_HOST", '127.0.0.1'), port=6379)
 
-@blueprint.route('/', methods=['GET', 'POST', 'PUT'])
-def endpoint():
-    if request.method == "GET":
-        # get(key)
-        key = request.args.get('key')
-        if key is not None:
-            value = redis.get(key)
+@blueprint.route('/variables', methods=['GET'])
+def get_variables():
+    return {key.decode('utf-8'): redis.get(key).decode('utf-8')
+                                    for key in redis.scan_iter()}
 
-            if value is None:
-                abort(404, "Key is not defined!")
-            else:
-                return {key: value.decode('utf-8')}
-        else:
-            # all()
-            return {key.decode('utf-8'): redis.get(key).decode('utf-8')
-                      for key in redis.scan_iter()}
 
-    elif request.method == "POST":
-        excepted = []
-        for key, value in request.args.items():
-            if redis.get(key) is None:
-                redis.set(name=key, value=value)
-            else:
-                excepted.append(key)
-        if len(excepted) > 0:
-            inline_excepted = ', '.join(excepted)
-            abort(412, f"Variables exists!\n\
-                        Variables: {inline_excepted} - exists in DB!")
-        else:
-            return {'status': 'Ok'}
+@blueprint.route('/variables/<key>', methods=['GET'])
+def get_variable(key):
+    value = redis.get(key)
 
-    elif request.method == "PUT":
-        exists = redis.exists(*list(request.args.keys()))
-
-        if exists == len(request.args.keys()):
-            for key, value in request.args.items():
-                redis.set(name=key, value=value)
-            return {'status': 'Ok'}
-        else:
-            abort(404, f"Variable not Found!\n")
-
+    if value is None:
+        abort(404)
     else:
-        abort(405, "Method not allowed!")
+        return {key: value.decode('utf-8')}
+
+
+@blueprint.route('/variables/', methods=['POST'])
+def set_variable():
+    if not request.json or not 'key' in request.json or not 'value' in request.json:
+        abort(400)
+
+    if redis.get(request.json.get('key')) is not None:
+        return jsonify({'error': 'Variable exists!'}), 400
+
+    key = request.json.get('key')
+    value = request.json.get('value')
+
+    redis.set(name=key, value=value)
+    return jsonify({key: value}), 201
+
+
+@blueprint.route('/variables/<key>', methods=['PUT'])
+def update_variable(key):
+    if not request.json or not 'value' in request.json:
+        abort(400)
+
+    if redis.get(key) is None:
+        return jsonify({'error': 'Variable not found!'}), 404
+
+    value = request.json.get('value')
+
+    redis.set(name=key, value=value)
+    return jsonify({key: value}), 200
