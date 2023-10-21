e70173ff9e6da02037a7e73d8e4a264f1aaa2c99
Yury Stolpov <yurass130@gmail.com>
2023-10-21 17:25:46 +0300

    Fix: Style fixes.

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-import os
-
-from flask import abort, Flask, request
-from redis import Redis
+from flask import Flask
 
 from blueprints.basic_endpoints import blueprint as basic_endpoints
 
 
 app = Flask(__name__)
 app.register_blueprint(basic_endpoints)
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port='8080', debug=True)
